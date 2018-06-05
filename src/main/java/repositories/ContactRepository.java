1c64d0e148d0f00495ca5a8378225e8af280f5b3
ronnyling <ronny.ling@accenture.com>
2018-06-05 19:22:40 +0800

    added controllers

--- src/main/java/repositories/ContactRepository.java
+++ src/main/java/repositories/ContactRepository.java
@@ -1,12 +1,18 @@
-package com.djamware.react.repositories;
+package repositories;
 
 
 import model.Contact;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 
 public interface ContactRepository extends CrudRepository<Contact, String> {
     @Override
     void delete(Contact deleted);
+
+    Optional<Contact> findById(String id);
+
+    Optional<Contact> getId(String id);
 }
